e3636bc8bfa69c99103ef77ef3b71758a5187e89
unknown <aneeshsharmachd@gmail.com>
2021-10-02 22:24:28 -0400

    React installed,format laid in App.js, Bootstrap Navbar added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./MyComponents/Header";
+import Todos from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
+
+
+function App() {
+  return (
+  <>
+    <Header/>  
+    <Todos/>
+    <Footer/>
+  </>
+  );
+}
+
+export default App;
---
+++ src/MyComponents/TodoItems.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TodoItems() 
+{
+    return (
+        <div>
+            todo works
+        </div>
+    )
+}
